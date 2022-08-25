4028c06234f1e44493f1ae66e20e1a48e410d9e0
rifat32 <rifatbilalphilips@gmail.com>
2022-08-25 17:27:56 +0600

    update

--- src/components/Forms/CitizenForm/AddCitizenForm.tsx
+++ src/components/Forms/CitizenForm/AddCitizenForm.tsx
@@ -711,7 +711,7 @@ return {
 					name="union_id"
 					onChange={handleSelect}
 					value={formData.union_id}>
-					<option value="">Please Select</option>
+					
 					{unions.map((el: any, index) => (
 						<option
 							key={index}
